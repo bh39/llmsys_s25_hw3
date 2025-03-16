d2ad88726774e35f77fce03e6bef911c337e7fa4
bh39 <bh39@rice.edu>
2025-03-15 20:57:56 -0400

    small fix

--- minitorch/tensor_functions.py
+++ minitorch/tensor_functions.py
@@ -419,7 +419,7 @@ class Attn_Softmax(Function):
     @staticmethod
     def forward(ctx: Context, inp: Tensor, mask: Tensor) -> Tensor:
       #   BEGIN ASSIGN3_1 
-      ctx.save_for_backward(inp.clone(), mask)
+      ctx.save_for_backward(inp.detach(), mask)
       result = inp.backend.attn_softmax_fw(inp, mask)
       
       return result
